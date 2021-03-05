93a4210c7c7ff2b73e08e19ca1cc01858cc94fd5
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2021-03-05 19:23:33 +0100

    Add some more experiments after trying to remember where we were last time

--- main.jl
+++ main.jl
@@ -2,13 +2,42 @@ using Distributions
 using Plots
 using LinearAlgebra
 
-function sampleData(β, σ², nsamples; hslope=0.0)
+function sampleData(β, σ², nsamples)
     x = rand(Normal(), nsamples);
-    hetero = 1.0 .+ hslope .* x
-    ϵ = hetero .* rand(Normal(0, σ²), nsamples);
+    ϵ = rand(Normal(0, σ²), nsamples);
     y = β*x + ϵ;
     
     x, y, ϵ
+end
+
+function sampleDataSigmaPrior(β, nsamples)
+    x = rand(Normal(), nsamples);
+    σ²ᵢ = rand(Chisq(1), nsamples)
+    ϵ = rand(Normal(0, σ²ᵢ), nsamples);
+    y = β*x + ϵ;
+    
+    x, y, ϵ
+end
+
+function sampleDataErrorCorrelatedToCovariates(β, γ, nsamples)
+    x = rand(Normal(), nsamples);
+    σ²ᵢ = γ*x + rand(Normal(0,1), nsamples)
+    ϵ = rand(Normal(0, σ²ᵢ), nsamples);
+    y = β*x + ϵ;
+    
+    x, y, ϵ
+end
+
+function sampleDataTwoVariances(β, σ²₁, σ²₂, nsamples)
+    x = rand(Normal(), nsamples);
+
+    σ²_idx = 1 .+ rand(Bernoulli(), nsamples) * 1
+    σ²ᵢ = [σ²₁; σ²₂][σ²_idx]
+
+    ϵ = sqrt.(σ²ᵢ) .* rand(Normal(0.0, 1.0), nsamples);
+    y = β*x + ϵ;
+    
+    x, y, ϵ, σ²ᵢ
 end
 
 function bootstrap(f, data, nbootstrap, bootstrapsamplesize)
@@ -35,9 +64,13 @@ bootstrapsamplesize = 800;
 x, y, ϵ = sampleData(β, σ², datasamplesize);
 data = hcat(x, y);
 
+# The OLS estimator of β follows sqrt(n) * (β - β̂) ~ N(0, σ²)
 β̂(data)
+
+# this estimates the variance of β̂, which should be equal to σ²/n = 1/n
 true_σ̂² = 1/dot(x, x)
 
+# this is a bootstrap estimator of the variance of  β̂
 function bootstrap_σ̂²(data)
     samples = bootstrap(β̂, data, nbootstrap, bootstrapsamplesize);
     var(samples)
@@ -45,16 +78,43 @@ end
 
 bootstrap_σ̂²(data)
 
-# we bootstrap the distribution of the bootstrap estimator
+# we bootstrap the distribution of the bootstrap estimator of the variance of β̂
 sample_σ̂² = bootstrap(_ -> bootstrap_σ̂²(data), data, nbootstrap, 1);
 mean(sample_σ̂²), var(sample_σ̂²)
 
 
 # heteroskedasticity
-x, y, ϵ = sampleData(β, σ², nsamples; hslope=1.0);
+# the true variance of each observation is either 1.0 or 2.0
+x, y, ϵ, Ω = sampleDataTwoVariances(β, 1.0, 2.0, datasamplesize);
 data = hcat(x, y);
 
 β̂(data)
+
+# so now, the OLS estimator of the variance of β̂ is wrong...
 1/dot(x, x)
 
+# ...which we can see when constructing the bootstrap estimate of it
 bootstrap_σ̂²(data)
+
+
+# beta_ols
+#     unbiased estimator of beta
+#     estimator of variance of beta_ols is s^2 / x'x
+#
+#     homoskedasticity
+#         ols estimator of variance of the beta_ols is consistent
+    
+#     heteroskedasticity
+#         ols estimator of variance of the beta_ols is  not consistent
+
+# beta_gls
+#     unbiased estimator of beta
+#     estimator of variance of the beta_gls is 
+
+
+
+
+# but if we know the real σ²ᵢ we can compute the gls
+1/dot(x, x./Ω)
+
+
